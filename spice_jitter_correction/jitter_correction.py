1faf4b02902bcfeb13131f3bfd2c1097046ad3e5
Gabriel Pelouze <gabriel@pelouze.net>
2022-07-15 16:45:56 +0200

    factor computation of interpolation points

--- spice_jitter_correction/jitter_correction.py
+++ spice_jitter_correction/jitter_correction.py
@@ -14,6 +14,49 @@ from .plot import PlotPointing
 from .utils import SpiceFilename
 
 
+def _get_spatial_wcs(header):
+    """ Get subset WCS for the spatial coordinates of a SPICE HDU.
+
+    Parameters
+    ==========
+    header : fits.Header
+        SPICE L2 FITS header.
+
+    Returns
+    =======
+    w : wcs.WCS
+        Spatial WCSa.
+    """
+    w = wcs.WCS(header)
+    w.wcs.pc[3, 0] = 0  # remove PC4_1 to decouple t from x
+    return w.sub(2)  # drop wavelength and time axes
+
+
+def _assert_hdus_have_the_same_spatial_coordinates(hdul):
+    """ Assert that SPICE image HDUs have the same spatial coordinates
+
+    Parameters
+    ==========
+    hdul : fits.HDUList
+        HDU list with images extensions.
+
+    Raises
+    ======
+    ValueError :
+        When the spatial coordinates of the HDUs differ.
+
+    """
+    w_ref = _get_spatial_wcs(hdul[0].header)
+    for hdu in hdul:
+        if hdu.is_image and hdu.name != 'WCSDVARR':
+            w = _get_spatial_wcs(hdu.header)
+            if w.to_header() != w_ref.to_header():
+                print(w_ref)
+                print(w)
+                raise ValueError('FITS extensions have different spatial '
+                                 'coordinates')
+
+
 def get_coordinates(header):
     """ Get SPICE coordinates (without distortion)
     
@@ -64,7 +107,37 @@ def add_distortion_to_coordinates(coordinates, hdul):
     return np.array([Tx - Tx_corr, Ty - Ty_corr])
 
 
-def remap_spice_hdu(hdu, hdul, sum_wvl=False):
+def get_interpolation_points(hdul):
+    """ Get the origin and target points for the interpolation
+
+    Parameters
+    ==========
+    hdul : fits.HDUList
+        HDU list with the image extensions for which to compute the
+        coordinates, plus two image extensions ``WCSDVARR`` (1 and 2)
+        containing the pointing distortion correction for `T_x` and `T_y`.
+
+    Returns
+    =======
+    points : array_like
+        Array of shape (ny*nx, 2) containing the coordinates of the data
+        points.
+    new_points : array_like
+        Array of shape (ny*nx, 2) containing the points at which to interpolate
+        the data.
+    """
+    _assert_hdus_have_the_same_spatial_coordinates(hdul)
+
+    coord_grid = get_coordinates(hdul[0].header)
+    coord_real = add_distortion_to_coordinates(coord_grid, hdul)
+
+    # Interpolation initial and target points
+    points = coord_real.reshape(2, -1).T
+    new_points = np.moveaxis(coord_grid, 0, -1)
+    return points, new_points
+
+
+def remap_spice_hdu(hdu, points, new_points, sum_wvl=False):
     """ Remap a SPICE spectral cube to corrected coordinates
 
     Parameters
@@ -72,9 +145,13 @@ def remap_spice_hdu(hdu, hdul, sum_wvl=False):
     hdu : fits.PrimaryHDU, fits.ImageHDU, fits.BinTableHDU
         SPICE L2 FITS HDU to remap. (If the HDU is not of 'image' type, return
         it without modification.)
-    hdul : fits.HDUList
-        Full FITS HDU, containing 'WCSDVARR' extensions with the pointing
-        distortion correction.
+    points : array_like
+        Array of shape (ny*nx, 2) containing the coordinates of the data
+        points.
+    new_points : array_like
+        Array of shape (ny*nx, 2) containing the points at which to interpolate
+        the data.
+    new_points : array_like
     sum_wvl : bool
         If True, sum along wavelength axis to generate a quicklook image.
 
@@ -85,13 +162,6 @@ def remap_spice_hdu(hdu, hdul, sum_wvl=False):
     """
     if not hdu.is_image or hdu.name == 'WCSDVARR':
         return hdu
-    # Get wcs coordinates from fits
-    coord_grid = get_coordinates(hdu.header)
-    coord_real = add_distortion_to_coordinates(coord_grid, hdul)
-
-    # Interpolation initial and target points
-    points = coord_real.reshape(2, -1).T
-    new_points = np.moveaxis(coord_grid, 0, -1)
 
     new_hdu = hdu.copy()
     if sum_wvl:
@@ -163,11 +233,12 @@ def correct_jitter(
 
     # open FITS
     hdulist = fits.open(filename.to_str())
+    points, new_points = get_interpolation_points(hdulist)
 
     # interpolate data
     new_hdulist = fits.HDUList(hdus=[])
     for hdu in hdulist:
-        new_hdu = remap_spice_hdu(hdu, hdulist, sum_wvl=sum_wvl)
+        new_hdu = remap_spice_hdu(hdu, points, new_points, sum_wvl=sum_wvl)
         new_hdulist.append(new_hdu)
 
     # save data
